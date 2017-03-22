49e1637a53be2255feeff9bccebf75effc668025
szcf-weiya <2215235182@qq.com>
2017-03-22 21:36:32 +0800

    add gdb

--- Cpp/pointerInStruct/test1.c
+++ Cpp/pointerInStruct/test1.c
@@ -29,28 +29,28 @@ int main(){
     */
     pt->p1 = malloc(sizeof(int));
     *(pt->p1) = 1;
-    *(pt->p1 + 5) = 1233;
+    *(pt->p1 +15) = 12233;
     int *ptmp = pt->p1;
     *(ptmp + 5) = 34;
     int i=0;
-    while(1)
-    {
-      if (ptmp == NULL)
-	break;
-      i = i + 1;
-    }
+    //while(1)
+    //{
+    //if (ptmp == NULL)
+    //	break;
+    // i = i + 1;
+    //}
     printf("%d\n",i);
     //    pt->p+1 = malloc(sizeof(char));
     //  pt->p[1] = 'q';
     //    pt->p[2] = 'p';
-    printf("&s = %x\n",pt->s); //等价于printf("%x\n", &(pt->s) );  
-    printf("&i = %x\n",&pt->i); //因为操作符优先级，我没有写成&(pt->i)  
+    //    printf("&s = %x\n",pt->s); //等价于printf("%x\n", &(pt->s) );  
+    //    printf("&i = %x\n",&pt->i); //因为操作符优先级，我没有写成&(pt->i)  
     printf("&c = %x\n",&pt->c);  
     printf("&p = %x\n",&pt->p);
     //    printf("p=%c\n",*(pt->p));
     //    printf("p=%c\n",*(pt->p+2));
     printf("p=%d\n",*(pt->p1));
-    printf("p=%d\n",*(pt->p1+5));
+    printf("p=%d\n",*(pt->p1+15));
     return 0;  
 }  
 /*
