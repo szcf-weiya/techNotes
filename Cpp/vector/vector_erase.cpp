1ccf84074098c94f5507960fa7bad93bb51f5e61
szcf-weiya <2215235182@qq.com>
2017-05-18 10:06:41 +0800

    add vector_erase

---
+++ Cpp/vector/vector_erase.cpp
@@ -0,0 +1,60 @@
+// ref: http://blog.sina.com.cn/s/blog_6377b8e60100ino6.html
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  vector <int> v1;
+  vector <int>::iterator Iter;
+  v1.push_back( 10 );
+  v1.push_back( 20 );
+  v1.push_back( 30 );
+  v1.push_back( 40 );
+  v1.push_back( 50 );
+  cout << "v1 =" ;
+  for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ ) 
+    cout << " " << *Iter;
+  cout << endl;
+  v1.erase( v1.begin( ) );
+  cout << "v1 =";
+  for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ ) 
+    cout << " " << *Iter;
+  cout << endl;
+  v1.erase( v1.begin( ) + 1, v1.begin( ) + 3 );
+  cout << "v1 =";
+ for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ ) 
+   cout << " " << *Iter;
+ cout << endl;
+ //当调用erase()后Iter迭代器就失效了，变成了一野指针。
+ //所以要处理这种问题，关键是要解决调用erase()方法后，Iter迭代器变成野指针的问题，
+ //这个时候呢给他赋一个新的迭代器给他。
+ v1.push_back( 10 );
+ v1.push_back( 30 );
+ v1.push_back( 10 );
+ cout << "v1 =";
+ for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ ) 
+   cout << " " << *Iter;
+ cout << endl; 
+ for(Iter = v1.begin(); Iter != v1.end(); Iter++) 
+   { 
+     if(*Iter == 10) 
+       { 
+	 v1.erase(Iter);
+	 Iter = v1.begin(); //当erase后，旧的容器会被重新整理成一个新的容器
+	 // or
+	 // Iter = v1.erase(Iter);
+       }
+     if (Iter == v1.end())
+       break;
+   }
+
+ cout << "v1 =";
+ for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ ) 
+   cout << " " << *Iter;
+ cout << endl; 
+ 
+ return 0;
+  
+}
---
+++ ssh/README.md
@@ -0,0 +1,20 @@
+# 建立SSH的信任关系
+
+- SSH公钥(~/.ssh/id_rsa.pub)
+Contains the public key for authentication.  These files are not sensitive and can (but need not) be readable by anyone.
+
+- 公钥授权文件(~/.ssh/authorized_keys)
+
+Lists the public keys (DSA/ECDSA/RSA) that can be used for logging in as this user.  The format of this file is described in the sshd(8) manual page.  This file is not highly sensitive, but the recommended permissions are read/write for the user, and not accessible by others.
+
+## 公钥和私钥
+
+- 加密是对数据进行处理，添加保护信息，如果非法用户得到被加密过的数据，也无法获取到原始的有效数据内容，所以加密的重点在于数据的安全性。
+- 认证是对数据进行处理，添加鉴权信息，如果在传输的过程中被非法篡改，接收方就会校验失败并丢弃该非法数据，所以认证的重点在于数据的合法性。
+
+## 基于公钥和私钥的加密过程
+
+## 基于公钥和私钥的认证过程
+
+## 基于公钥和私钥的信任关系
+
