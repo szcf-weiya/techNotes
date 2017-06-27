f2f2ad570961da0359b7a739ea8033a933d39bf9
szcf-weiya <2215235182@qq.com>
2017-06-27 22:07:31 +0800

    add gamma smc

---
+++ scientific/gamma_from_smc.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+double gammaln(double xx) //computes the log of the gamma function
+{
+	double x,y,tmp,ser;
+	static double cof[6] ={76.18009172947146,-86.50532032941677,
+		24.01409824083091,-1.231739572450155,
+		0.1208650973866179e-2,-0.5395239384953e-5};
+	int j;	
+	
+	y = x = xx;
+	tmp = x+5.5;
+	tmp -= (x+0.5)*log(tmp);
+	ser = 1.000000000190015;
+	for (j=0;j<=5;j++)
+	{
+		ser += cof[j]/++y;
+	}
+	return((double) (-tmp+log(2.5066282746310005*ser/x)));
+}
+
+
+int main()
+{
+  for (int i = 0; i < 10; i++)
+    cout << gammaln(i+0.001) << endl;
+  return 0;
+}
