3fb1955e65a5493527b990200aaf4bdae46c5a5d
szcf-weiya <szcf-weiya@users.noreply.github.com>
2021-11-21 01:55:53 +0000

    deploy: d3613af27ded5b527a1c668eec9abc0681a02523

--- julia/util.jl
+++ julia/util.jl
@@ -6,3 +6,21 @@ function save_plots(ps)
     fignames = "/tmp/p" .* string.(1:n) .* ".pdf"
     run(`pdftk $fignames cat output /tmp/all.pdf`)
 end
+
+function save_grid_plots(ps, nrow, ncol)
+    n = length(ps)
+    if nrow * ncol != n
+        error("different number of plots")
+    end
+    for (i, p) in enumerate(ps)
+        savefig(p, "/tmp/p$i.png")
+    end
+    for i = 1:nrow
+        fignames = "/tmp/p" .* string.(ncol * (i-1) .+ (1:ncol)) .* ".png"
+        run(`convert $fignames +append /tmp/pp$i.png`)
+    end
+    for i = 1:nrow
+        fignames = "/tmp/pp" .* string.(1:nrow) .* ".png"
+        run(`convert $fignames -append /tmp/all.png`)
+    end
+end
