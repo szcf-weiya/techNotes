793c1b729becc582a711cb6be773c06e01a23d6f
szcf-weiya <szcf-weiya@users.noreply.github.com>
2021-12-02 04:29:45 +0000

    deploy: 9b00c6c450cf845fab74c658aedfd584edeeb37e

--- julia/util.jl
+++ julia/util.jl
@@ -43,3 +43,50 @@ function save_grid_plots(ps, nrow, ncol, out = "all")
         run(`convert $fignames -append /tmp/$out.png`)
     end
 end
+
+function myparse(str)
+    if occursin(":", str) # only for int currently
+        ys = split(str, ":")
+        xs = parse.(Int, ys)
+        if length(xs) == 2
+            return xs[1]:xs[2]
+        else
+            return xs[1]:xs[2]:xs[3]
+        end
+    else
+        if occursin(".", str)
+            return parse(Float64, str)
+        else
+            return parse(Int, str)
+        end
+    end
+end
+
+"""
+    split_keystr(str)
+
+Split string with format `StrNum_StrNum` into key pairs
+
+```jldoctest
+julia> split_keystr("a123_b234")
+Dict{Any, Any} with 2 entries:
+  "b" => 234
+  "a" => 123
+```
+"""
+function split_keystr(x)
+    # x = "a123_b234"
+    xs = split(x, "_")
+    res = Dict()
+    for y in xs
+        try
+            ys = match(r"([a-zA-Z]+)(.*)", y).captures
+            if ys[2] != ""
+                res[ys[1]] = myparse(ys[2])
+            end
+        catch e
+            @warn e
+        end
+    end
+    return res
+end
