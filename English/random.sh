441213251bc442ee142396788e08bcd1c9fecee3
szcf-weiya <szcf-weiya@users.noreply.github.com>
2022-08-06 15:59:10 +0000

    deploy: 5a09ae26fdbec8a645db2caf7a144d65564922a8

---
+++ English/random.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+txt=$(sed -n "$(($RANDOM % $(wc -l < index.md) + 1))p" index.md)
+# https://stackoverflow.com/questions/13509508/check-if-string-is-neither-empty-nor-space-in-shell-script
+if [ -n ${txt// /} ]; then
+    if [ ${txt:0:1} != '#' ]; then
+        notify-send -t 10 ' ' "${txt:1}"
+    fi
+fi
